c007adeffc5198d8c83b790a2175cb6fe43fc242
Bartosz Glowacki <152788864+BGlowacki-kcl@users.noreply.github.com>
2024-07-28 09:41:44 +0100

    Update models.py

--- health/models.py
+++ health/models.py
@@ -1,17 +1,24 @@
 from django.db import models
 import datetime
-from django.contrib.auth.models import User
 
 class Days(models.Model):
     day = models.CharField(max_length=15)
     def __str__(self):
         return self.day
 
+class MyUser(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    imageURl = models.CharField(max_length=255, null=True, blank=True)
+    bio = models.CharField(max_length=255, null=True, blank=True)
+
 class Habit(models.Model):
     name = models.CharField(max_length=20, default="")
     description = models.CharField(max_length=255, default="")
     consistency = models.ManyToManyField(Days, default=["Monday"])
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, blank=True, null=True)
     def __str__(self):
         return self.name
 
